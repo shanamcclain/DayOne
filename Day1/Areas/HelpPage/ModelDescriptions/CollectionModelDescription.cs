7b53e364b926ecf1f3448e78cef8b9981b29c1df
shanamcclain <35242747+shanamcclain@users.noreply.github.com>
2018-02-06 09:52:33 -0500

    Initial commit

---
+++ Day1/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Day1.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
